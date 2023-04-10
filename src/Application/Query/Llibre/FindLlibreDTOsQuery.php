5d54d5b9239f2ba6cf44d1559b4cef9d8af56760
Rubén Rubio <ruben.rubio.barrera@gmail.com>
2023-04-10 12:30:58 +0200

    Afegir FindLlibreDTOs Query + Handler

---
+++ src/Application/Query/Llibre/FindLlibreDTOsQuery.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace rubenrubiob\Application\Query\Llibre;
+
+final readonly class FindLlibreDTOsQuery
+{
+    public function __construct()
+    {
+    }
+}
