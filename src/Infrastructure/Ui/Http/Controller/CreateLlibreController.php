12bd2003ceee11852ad4939d9dc0918325dd1990
Rubén Rubio <ruben.rubio.barrera@gmail.com>
2023-04-14 13:32:34 +0200

    Refactoritzar CreateLlibreController

--- src/Infrastructure/Ui/Http/Controller/CreateLlibreController.php
+++ src/Infrastructure/Ui/Http/Controller/CreateLlibreController.php
@@ -6,62 +6,21 @@ namespace rubenrubiob\Infrastructure\Ui\Http\Controller;
 
 use rubenrubiob\Application\Command\Llibre\CrearLlibreCommand;
 use rubenrubiob\Infrastructure\CommandBus\CommandBus;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-
-use function array_key_exists;
-use function is_string;
+use rubenrubiob\Infrastructure\Ui\Http\Request\CreateLlibreRequestBody;
 
 final readonly class CreateLlibreController
 {
-    private const KEY_TITOL = 'titol';
-    private const KEY_AUTOR = 'autor';
-
     public function __construct(private CommandBus $commandBus)
     {
     }
 
-    public function __invoke(Request $request): void
+    public function __invoke(CreateLlibreRequestBody $createLlibreRequestBody): void
     {
-        $requestContent = $this->parseAndGetRequesContent($request);
-
         $this->commandBus->__invoke(
             new CrearLlibreCommand(
-                $requestContent[self::KEY_TITOL],
-                $requestContent[self::KEY_AUTOR],
+                $createLlibreRequestBody->titol,
+                $createLlibreRequestBody->autor,
             )
         );
-    }
-
-    /**
-     * @return array{
-     *      titol: string,
-     *      autor: string,
-     *     ...
-     * }
-     *
-     * @throws BadRequestHttpException
-     */
-    private function parseAndGetRequesContent(Request $request): array
-    {
-        $requestContent = $request->toArray();
-
-        if (!array_key_exists(self::KEY_TITOL, $requestContent)) {
-            throw new BadRequestHttpException('Missing "titol"');
-        }
-
-        if (!is_string($requestContent[self::KEY_TITOL])) {
-            throw new BadRequestHttpException('"titol" format is not valid');
-        }
-
-        if (!array_key_exists(self::KEY_AUTOR, $requestContent)) {
-            throw new BadRequestHttpException('Missing "autor"');
-        }
-
-        if (!is_string($requestContent[self::KEY_AUTOR])) {
-            throw new BadRequestHttpException('"autor" format is not valid');
-        }
-
-        return $requestContent;
     }
 }
